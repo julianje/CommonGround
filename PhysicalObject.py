3159526321f08f3439558e430fc8101d1b36722c
julianje <jjara@mit.edu>
2017-02-12 23:49:59 -0500

    Simple representation of physical objects

---
+++ PhysicalObject.py
@@ -0,0 +1,9 @@
+class PhysicalObject:
+
+    def __init__(self, name, feature, featuretype):
+        """
+        Save an object's name together with a feature and a feature type
+        """
+        self.name = name
+        self.feature = feature
+        self.featuretype = featuretype
