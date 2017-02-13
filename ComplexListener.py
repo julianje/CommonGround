46f63efbc69bdddd09065979d374597f91457ac6
julianje <jjara@mit.edu>
2017-02-12 23:50:56 -0500

    temp complex listener class

---
+++ ComplexListener.py
@@ -0,0 +1,15 @@
+class ComplexListener:
+
+    def __init__(self, Bias, VisualWorld, CommonGroundPrior):
+        """
+        Bias and VisualWorld are objects of that type.
+        CommonGroundPrior is a list of the same length as
+        the number of objects in the visual world and they
+        indicate the probability that the object is
+        common ground.
+        """
+        self.Bias = Bias
+        self.VisualWorld
+        # Sanity check that BeliefTracker makes sense
+
+    def InferReferent(self):
