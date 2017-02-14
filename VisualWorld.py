f36a5e3787ca416a5e41f518a81d174f9a686131
julianje <jjara@mit.edu>
2017-02-13 20:37:14 -0500

    Better functionality

--- VisualWorld.py
+++ VisualWorld.py
@@ -10,5 +10,26 @@ class VisualWorld:
         self.objects = objects
 
     def Contains(self, target):
-        res = 1 if sum([x == target for x in self.objects]) > 0 else 0
+        res = 1 if sum([(x.name == target.name and x.feature == target.feature)
+                        for x in self.objects]) > 0 else 0
         return res
+
+    def GetIndex(self, target):
+        """
+        Get index of a target in vector list.
+        This is helpful because sometimes you want to
+        identify an object by using an isomoprhic object
+        from a different visual world object. In those cases
+        direct comparisons don't work
+        """
+        match = [(x.name == target.name and x.feature == target.feature) for x in self.objects]
+        return match.index(1)
+
+    def Delete(self, target):
+        """
+        remove target from list of objects
+        """
+        if self.Contains(target):
+            self.objects.pop(self.GetIndex(target))
+        else:
+            print "Nothing in list."
