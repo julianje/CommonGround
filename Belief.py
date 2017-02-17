0a141dcbff2a483740c6d4d1f5c72b66ebbe40f9
julianje <jjara@mit.edu>
2017-02-17 16:30:48 -0500

    added naming system

--- Belief.py
+++ Belief.py
@@ -7,20 +7,40 @@ class Belief:
     Simple class to store belief distributions
     """
 
-    def __init__(self, HypothesisSize, prior=None):
+    def __init__(self, HypothesisSize, names=None, prior=None):
         """
         Generate a belief distribution of size HypothesisSize.
         By default a uniform prior is built. But if a vector with priors is provided
         then that one is used.
+
+        Args:
+        HypothesisSize (int): Number of hypothesis
+        names (list): Optional list of names of each hypothesis
+        prior (list): optional list indicating the prior
         """
         if prior is not None:
             if len(prior) != HypothesisSize:
                 print "ERROR: Warning has a different size than hypothesis space size"
             else:
-                self.prior = prior
+                self.probs = prior
         else:
-            self.prior = [1.0/HypothesisSize] * HypothesisSize
+            self.probs = [1.0/HypothesisSize] * HypothesisSize
+        if names is not None:
+            if len(names) != HypothesisSize:
+                print "ERROR: List of names does not match hypothesis size."
+            else:
+                self.names = names
+        else:
+            self.names = ["Item "+str(x) for x in range(HypothesisSize)]
 
     def Normalize(self):
-        Norm = sum(self.prior)
-        self.prior = [x*1.0/Norm for x in self.prior]
+        Norm = sum(self.probs)
+        self.probs = [x*1.0/Norm for x in self.probs]
+
+    def Certain(self):
+        """
+        Return true if distribution has a 1 in one place and zero in all other places
+        """
+        if sum(self.probs) != 1:
+            self.Normalize
+        return True if sum([x == 1 for x in self.probs]) == 1 else False
